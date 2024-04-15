d1771620f10a8b72be50ab0354fa845ba8c031e5
thame <thame@stakersunion.com>
2024-04-14 18:03:40 -0700

    Update home page
    Added secondary banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 import { Container, Logo } from '@/components'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <Container>
       <Logo />
-      <h1>Home</h1>
+      <div className={'my-8 text-center'}>
+        <h2 className={'font-bold text-2xl text-muted-foreground'}>
+          A collective of <span className={'text-foreground'}>home-stakers</span> for the Ethereum
+          network
+        </h2>
+        <Button>
+          Unionize
+        </Button>
+      </div>
     </Container>
   )
 }
